35da1d7a9b42e5d4d8b31b9b7282222c513c861f
csimeone23 <csimeone1223@gmail.com>
2021-10-25 22:13:07 -0400

    Added files

--- Hubserver.c
+++ Hubserver.c
@@ -2,38 +2,80 @@
 #include <stdlib.h>
 #include <winsock2.h>
 
-SOCKADDR_IN known_connections[64];
-int connection = 0;
+#define MAX_CONNECTION_NUM 64
 
-int main(int argc, char *argv[]){
-    WSADATA wsadata;
-    int err_code = WSAStartup(MAKEWORD(2, 2), &wsadata);
-    if(err_code != 0){
-        printf("WSAStartup failed with the error %s\n", err_code);
+SOCKADDR_IN known_connections[MAX_CONNECTION_NUM];
+int connections = 0;
+
+void handleSetup(WSADATA *wsadata, SOCKET *main_socket, SOCKADDR_IN *addr){
+    int error_code = WSAStartup(MAKEWORD(2, 2), wsadata);
+    if( error_code != 0){
+        printf("WSAStartup failed with the error %s\n", error_code);
         exit(EXIT_FAILURE);
     }
+    printf("WSAStartup Successful!\n");
 
-    SOCKET main_socket = socket(AF_INET, SOCK_DGRAM, 0);
-    if(main_socket == INVALID_SOCKET){
+    *main_socket = socket(AF_INET, SOCK_DGRAM, 0);
+    if(*main_socket == INVALID_SOCKET){
         printf("ERROR CREATING SOCKET\n\tERROR CODE: %d\n", WSAGetLastError());
         WSACleanup();
         exit(EXIT_FAILURE);
     }
+    printf("Socket created successfully!\n");
 
-    SOCKADDR_IN addr;
-    addr.sin_family = AF_INET;
-    addr.sin_addr.s_addr = inet_addr("192.168.1.13");
-    addr.sin_port = htons(25565);
-    int binder = bind(main_socket, (SOCKADDR *) &addr, sizeof(addr));
-    if(binder == SOCKET_ERROR){
-        printf("ERROR BINDING SOCKET: CODE %d", WSAGetLastError());
+    addr->sin_family = AF_INET;
+    addr->sin_addr.s_addr = inet_addr("192.168.1.13");
+    addr->sin_port = htons(25565);
+
+    if( bind(*main_socket, (SOCKADDR *) addr, sizeof(*addr)) == SOCKET_ERROR){
+        printf("Error binding socket: CODE %d\n", WSAGetLastError());
         WSACleanup();
         exit(EXIT_FAILURE);
     }
+    printf("Socket binded successfully!\n");
+    return;
+}
 
-    SOCKADDR_IN incoming_socket;
-    int recv_len;
-    int slen = sizeof(incoming_socket);
+int handleIntroductoryCheck(char *buf, SOCKADDR_IN *incoming_socket){
+    for(int i=0; i<connections; i++){
+        if(known_connections[i].sin_addr.s_addr == incoming_socket->sin_addr.s_addr)
+            return 0;
+    }
+    return 1;
+}
+
+void handleReceivedData(char *buf, SOCKET *main_socket, SOCKADDR_IN *incoming_socket, int *recv_len, int *slen){
+    if(strcmp(buf, "talk to me shawty") == 0 && handleIntroductoryCheck(buf, incoming_socket) == 1){
+            printf("We got an introductory request\nAdding IP to known_connections");
+            known_connections[connections] = *incoming_socket;
+            connections++;
+    } else {
+        int index = 0;
+        while( ntohs(known_connections[index].sin_port) != 0 && index < connections && index < MAX_CONNECTION_NUM){
+            SOCKADDR_IN current_connection = known_connections[index];
+
+            if(ntohl(current_connection.sin_addr.s_addr) != ntohl(incoming_socket->sin_addr.s_addr)){
+                if( sendto(*main_socket, buf, *recv_len, 0, (struct sockaddr*) &current_connection, *slen) == SOCKET_ERROR ){
+                    printf("ERROR SENDING DATA TO CLIENT %s\nERROR CODE:%d", inet_ntoa(current_connection.sin_addr), WSAGetLastError());
+                    index++;
+                } else {
+                    printf("Data sent to %s was successful!\n", inet_ntoa(current_connection.sin_addr));
+                }
+            } else {
+                printf("skipping IP because it is the original sender of the datagram\n");
+            }
+            index++;
+        }
+    }
+}
+
+int main(int argc, char *argv[]){
+    WSADATA wsadata;
+    SOCKET main_socket;
+    SOCKADDR_IN addr, incoming_socket;
+    handleSetup(&wsadata, &main_socket, &addr);
+
+    int recv_len, slen = sizeof(incoming_socket);
     while(TRUE){
         printf("Waiting for data...\n");
         char *buf = calloc(512, sizeof(char));
@@ -44,30 +86,7 @@ int main(int argc, char *argv[]){
         }
         printf("Received packet from %s:%d\n", inet_ntoa(incoming_socket.sin_addr), ntohs(incoming_socket.sin_port));
         printf("Data: %s\n", buf);
-
-        if(strcmp(buf, "talk to me shawty") == 0){
-            printf("We got an introductory request\nAdding IP to known_connections");
-            known_connections[connection] = incoming_socket;
-            connection++;
-        } else {
-            int index = 0;
-            while( ntohs(known_connections[index].sin_port) != 0 && index < 64){
-                SOCKADDR_IN current_connection = known_connections[index];
-
-                if(ntohl(current_connection.sin_addr.s_addr) != ntohl(incoming_socket.sin_addr.s_addr)){
-                    int send_to = sendto(main_socket, buf, recv_len, 0, (struct sockaddr*) &current_connection, slen);
-                    if( send_to == SOCKET_ERROR ){
-                        printf("ERROR SENDING DATA TO CLIENT %s\nERROR CODE:%d", inet_ntoa(current_connection.sin_addr), WSAGetLastError());
-                        index++;
-                        continue;
-                    }
-                    printf("Data sent to %s was successful!\n", inet_ntoa(current_connection.sin_addr));
-                } else {
-                    printf("skipping IP because it is the original sender of the datagram\n");
-                }
-                index++;
-            }
-        }
+        handleReceivedData(buf, &main_socket, &incoming_socket, &recv_len, &slen);
         free(buf);
         free(&incoming_socket);
     }
